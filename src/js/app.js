1b9d1a5bd25bcff0a9d8ae30ba2170020a8ecb1c
kiparenkomb <kiparenkomb@gmail.com>
2017-06-19 07:36:26 +0300

    v1.1

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,71 @@ import HP from './helpers';
 
 (function ($) {
 
-  // When DOM is ready
-  $(function () {
-    example(HP.random(10, 20));
-  });
+// function stickyMenu() {
 
-  /**
-   * Just an example function
-   *   DELETE IT
-   * @param  {Number} n - random number between 10 and 20
-   */
-  function example(n) {
-    console.log(`Hello in ES6... I am ${n} - random number between 10 and 20`);
-  }
+	$(document).scroll(function () {
+		if ($(this).scrollTop() > 0) {
+			$('#navbar').addClass('navbar-small');
+		} else {
+			$('#navbar').removeClass('navbar-small');
+		}
+	});
+// }
+
+	// SLIDER SECTION HOME
+	$('.text-slider').slick({
+			autoplay: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: false,
+			speed: 900,
+			cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+			draggable: true,
+			responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+
+	// ANIMATIONS
+	$('.service-item:even').viewportChecker({
+		classToAdd: 'fadeInDown',
+		// repeat: true,
+		offset: -150,
+	});
+	$('.service-item:odd').viewportChecker({
+		classToAdd: 'fadeInUp',
+		// repeat: true,
+		offset: -150,
+	});
+	$('.process-item').viewportChecker({
+		classToAdd: 'fadeInLeft',
+		repeat: true,
+		// offset: -150,
+	});
+	
+	// $('.about__item:nth-child(odd)').viewportChecker({
+	// 	classToAdd: 'fadeInLeft',
+	// });
+	// $('.about__item:nth-child(even)').viewportChecker({
+	// 	classToAdd: 'fadeInRight',
+	// });
+	// $('.info-box').viewportChecker({
+	// 	classToAdd: 'info-visible',
+	// });
+
+
+
+
+
+
+
 
 }(jQuery));
--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,3 +1,10 @@
 module.exports = [
-  './bower/jquery/dist/jquery.js'
+	'./bower/jquery/dist/jquery.js',
+	'./bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+	'./bower/Counter-Up/jquery.counterup.js',
+	'./bower/slick-carousel/slick/slick.js',
+	'./bower/jQuery-viewport-checker/dist/jquery.viewportchecker.min.js',
+
+	// './bower/jquery-ui/jquery-ui.js',
+	// './bower/jquery-validation/dist/jquery.validate.js',
 ];
